4cac8eefc726bb5d2bf2de6c8bd20c4ae6ac6750
nuscheltier <jonas@farel.de>
2018-01-14 22:48:42 +0100

    Added categories as one-fits-all. See Comment inside the class.

---
+++ src/humin/categories.rs
@@ -0,0 +1,9 @@
+//Categories are the go to place for tags and things like that. It's a one-fits-all.
+//Effectively you can use it for Clans, Organizations and other things.
+
+pub struct Categories {
+    categories: Vec<u64>
+}
+
+impl Categories {
+}
