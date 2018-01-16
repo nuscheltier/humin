d82417e186c721fdd4b935d0b8daab986bd7c16a
nuscheltier <jonas@farel.de>
2018-01-16 11:47:34 +0100

    added orgnisations and groups

--- src/humin/mod.rs
+++ src/humin/mod.rs
@@ -2,3 +2,4 @@ pub mod calendar;
 pub mod character;
 pub mod place;
 pub mod event;
+pub mod organisations;
--- src/humin/organisations.rs
+++ src/humin/organisations.rs
@@ -1,4 +1,22 @@
+use super::super::database::db::DB as DB;
+//Groups is the struct to collect all Organisations. It's named to make it differentiate more
+//from Organisations.
+pub struct Groups {
+    groups: Vec<Organisation>
+}
 //Organisations should have a starting Event (their founding) and some people (the founders).
 //But for worldbuilding purposes it should be optional.
-pub struct Organisations {
+pub struct Organisation {
+    node_id: u64
+}
+
+impl Groups {
+}
+
+impl Organisation {
+    pub fn new(db: &mut DB) -> Organisation {
+        Organisation {
+            node_id: db.new_node()
+        }
+    }
 }
